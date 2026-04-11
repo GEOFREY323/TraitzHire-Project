160c876ab1f9a5fb189c6c630e12c6ec29c50569
GEOFREY323 <gamgeofreyankinimbom@gmail.com>
2026-04-11 14:59:50 +0100

    update allow host

--- traitzhire_project/settings.py
+++ traitzhire_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #Reads SECRET_KEY from .env, raises error if missing
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','traitzhire-project.onrender.com',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','traitzhire-project-production.up.railway.app',]
 CSRF_TRUSTED_ORIGINS = [
-    'https://traitzhire-project.onrender.com',  # note the https://
+    'https://traitzhire-project-production.up.railway.app',  # note the https://
     'http://127.0.0.1:8000',
 ]
 
